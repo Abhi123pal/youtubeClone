71fa9b69f77edb36913af3b1d0189cb7f0a83321
Abhi123pal <abhinavpal889@gmail.com>
2023-03-07 16:00:42 +0530

    Initial commit

---
+++ src/components/VideoCardContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import VideoCard from "./VideoCard";
+import "../App.css";
+const VideoCardContainer = () => {
+  return (
+    <div className="mainContainer">
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+      <VideoCard />
+    </div>
+  );
+};
+
+export default VideoCardContainer;
